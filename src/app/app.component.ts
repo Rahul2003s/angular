c70d91574c2e43a366e46e74810db6d2a0525410
Rahul S <rahulsri073@gmail.com>
2022-11-21 16:11:21 +0530

    inter polation-- property binding---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'count';
-}
+  c : number = 0;
+  val : string = "";
+  dis : boolean = true;
+  increment() {
+    this.dis=false;
+    this.c=this.c+1;
+  }
+  decriment(){
+    if(this.c==1){
+      this.dis=true;
+      this.c=0;
+    }else{
+      this.c=this.c-1;
+    }
+  }
+  reset(){
+    this.dis=true;
+    this.c=0
+  }
+
+  print_name(c:string){
+    this.val=c
+  }
+  }
+
+  
+
+
